f16c2f71f364eaf7364a1d1d2d721284d8a483ee
karimchalhoub <karim.chalhoub@gmail.com>
2017-11-09 18:48:36 -0800

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -90,7 +90,29 @@ var UserSchema = new mongoose.Schema({
     } else {
       next();
     }
+  });
+
+  UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({
+      'email': email
+    }).then((user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject();
+          }
+        });
+      });
     });
+  };
+
 
 var User = mongoose.model('User', UserSchema);
 
